d92d0cae849dde7607ecca048b28660b3c935be5
Jonas <s224941@student.dtu.dk>
2025-02-24 10:25:25 +0100

    lagt til litt om meg selv:D

--- download.py
+++ download.py
@@ -3,3 +3,5 @@ import nltk
 
 nltk.download('stopwords')
 nltk.download('punkt_tab')
+
+#My name is jonas and I am a student at the university of applied sciences in Salzburg. I am currently working on a project in the field of natural language processing. I am trying to build a chatbot that can answer questions about the weather. I am using Python and the Natural Language Toolkit library to build the chatbot. I am also using the OpenWeatherMap API to get the weather data. I am excited to see how the chatbot turns out and I hope it will be able to help people get the information they need about the weather.
