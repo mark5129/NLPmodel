31f6cf1924921c46bf895fe561f1f93c338851ee
mark5129 <markus.kaad.heuer@gmail.com>
2025-03-20 18:03:32 +0100

    Ran manually with new embeddings from 20 march

--- Visualizations/main_datamapplot.py
+++ Visualizations/main_datamapplot.py
@@ -7,23 +7,36 @@ from topic_source_plot import topic_source_plot
 
 
 #Load embeddings from embeddings file
-which_model = ['XLM_Roberta', 'Specter2', 'MiniLm12', 'BERTopic']
+which_model = ['XLM_Roberta', 'Specter2Actually', 'MiniLm12']
 
-# Load the merged media file to get texts
-file = ['modelling/outputs/XLM_Roberta/2418438043_merged_XLM_Roberta_embeddings.csv',
-        'modelling/outputs/Specter2/2418438043_merged_Specter2_embeddings.csv',
-        'modelling/outputs/MiniLm12/2418438043_merged_MiniLm12_embeddings.csv',
-        'modelling/outputs/BERTopic/2418438043_merged_BERTopic_embeddings.csv',
+merged_file = ['modelling/outputs/XLM_Roberta/6751423855_merged_XLM_Roberta_embeddings.csv',
+        'modelling/outputs/Specter2Actually/6751423855_merged_Specter2Actually_embeddings.csv',
+        'modelling/outputs/MiniLm12/6751423855_merged_MiniLm12_embeddings.csv',
         'evaluations/outputs/manualrun_merged_XLM_Roberta_Kmeans.csv',
-        'evaluations/outputs/manualrun_merged_Specter2_Kmeans.csv',
-        'evaluations/outputs/manualrun_merged_MiniLm12_Kmeans.csv',
-        'evaluations/outputs/manualrun_merged_BERTopic_Kmeans.csv']
+        'evaluations/outputs/manualrun_merged_Specter2Actually_Kmeans.csv',
+        'evaluations/outputs/manualrun_merged_MiniLm12_Kmeans.csv']
+
+embed_merged_file = ['modelling/outputs/XLM_Roberta/6751423855_merged_embeddings_XLM_Roberta_embeddings.csv',
+        'modelling/outputs/Specter2Actually/6751423855_merged_embeddings_Specter2Actually_embeddings.csv',
+        'modelling/outputs/MiniLm12/6751423855_merged_embeddings_MiniLm12_embeddings.csv',
+        'evaluations/outputs/manualrun_merged_embeddings_XLM_Roberta_Kmeans.csv',
+        'evaluations/outputs/manualrun_merged_embeddings_Specter2Actually_Kmeans.csv',
+        'evaluations/outputs/manualrun_merged_embeddings_MiniLm12_Kmeans.csv']
+
 
 df_file = pd.read_csv('data/merged_media_stemmed_eng.csv')
 
 for i in range(len(which_model)):
-    embeddings = pd.read_csv(file[i])
-    kmeans = pd.read_csv(file[i+len(which_model)])
+    embeddings = pd.read_csv(merged_file[i])
+    kmeans = pd.read_csv(merged_file[i+len(which_model)])
     model = which_model[i]
     data_mapplot_with_naming(embeddings, df_file, 'manualrun', 'merged', model)
     topic_source_plot(kmeans, 'manualrun', 'merged', model)
+
+
+for i in range(len(which_model)):
+    embeddings = pd.read_csv(embed_merged_file[i])
+    kmeans = pd.read_csv(embed_merged_file[i+len(which_model)])
+    model = which_model[i]
+    data_mapplot_with_naming(embeddings, df_file, 'manualrun', 'merged_embeddings', model)
+    topic_source_plot(kmeans, 'manualrun', 'merged_embeddings', model)
--- evaluations/Eval_manual.py
+++ evaluations/Eval_manual.py
@@ -1,17 +1,17 @@
 import pandas as pd
 
-from BERTopic_cluster_topics import K_means_clustering
+from K_means_clustering import K_means_clustering
 
 # This script only runs on embeddings from various models.
 
 
 #Load embeddings from embeddings file
-which_model = ['XLM_Roberta', 'Specter2', 'MiniLm12']
+which_model = ['XLM_Roberta', 'Specter2Actually', 'MiniLm12']
 
 # Load the merged media file to get texts
-file = ['modelling/outputs/XLM_Roberta/2418438043_merged_XLM_Roberta_embeddings.csv',
-        'modelling/outputs/Specter2/2418438043_merged_Specter2_embeddings.csv',
-        'modelling/outputs/MiniLm12/2418438043_merged_MiniLm12_embeddings.csv']
+file = ['modelling/outputs/XLM_Roberta/6751423855_merged_XLM_Roberta_embeddings.csv',
+        'modelling/outputs/Specter2Actually/6751423855_merged_Specter2Actually_embeddings.csv',
+        'modelling/outputs/MiniLm12/6751423855_merged_MiniLm12_embeddings.csv']
 
 df_file = pd.read_csv('data/merged_media_stemmed_eng.csv')
 
@@ -19,3 +19,12 @@ for i in range(len(which_model)):
     embeddings = pd.read_csv(file[i])
     model = which_model[i]
     K_means_clustering(embeddings, df_file, 'manualrun', 'merged', model)
+
+file = ['modelling/outputs/XLM_Roberta/6751423855_merged_embeddings_XLM_Roberta_embeddings.csv',
+        'modelling/outputs/Specter2Actually/6751423855_merged_embeddings_Specter2Actually_embeddings.csv',
+        'modelling/outputs/MiniLm12/6751423855_merged_embeddings_MiniLm12_embeddings.csv']
+
+for i in range(len(which_model)):
+    embeddings = pd.read_csv(file[i])
+    model = which_model[i]
+    K_means_clustering(embeddings, df_file, 'manualrun', 'merged_embeddings', model)
