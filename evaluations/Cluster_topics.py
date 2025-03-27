a013bee827c75734ec8574164ba828e48adb2e14
fjeldsimen <s224920@student.dtu.dk>
2025-03-27 17:48:02 +0100

    comment noise code

--- evaluations/Cluster_topics.py
+++ evaluations/Cluster_topics.py
@@ -51,7 +51,7 @@ def clustering_and_naming(embeddings, df_file, run_id, doc, model):
     )
 
     # Filter out noise (topic_int = -1)
-    result_df = result_df[result_df['topic_int'] != -1]  # Uncomment this line if you want to remove noise points
+    # result_df = result_df[result_df['topic_int'] != -1]  # Uncomment this line if you want to remove noise points
 
     # Replace underscores with commas in 'labels_layer'
     result_df['labels_layer'] = result_df['labels_layer'].str.replace('_', ', ')
