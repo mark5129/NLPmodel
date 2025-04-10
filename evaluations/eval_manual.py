b342948d9d5e264ae1790a5e0bcc183c0f9ccdab
fjeldsimen <s224920@student.dtu.dk>
2025-04-10 15:10:05 +0200

    love

--- evaluations/eval_manual.py
+++ evaluations/eval_manual.py
@@ -10,7 +10,7 @@ with open('parameters.yaml', 'r') as file:
     parameters = yaml.safe_load(file)
 
 #Load embeddings from embeddings file
-which_model = ['Specter2Actually']
+which_model = ['XLM_Roberta', 'Specter2Actually', 'MiniLm12']
 sources = ['pro', 'reg', 'sci']
 
 # Determine the latest run ID to know which embeddings to use
