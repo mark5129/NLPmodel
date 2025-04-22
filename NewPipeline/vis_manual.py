5f4896a08e9a078f8770baca1e1ce01b78644981
mark5129 <markus.kaad.heuer@gmail.com>
2025-04-22 10:48:21 +0200

    Merged plot

--- NewPipeline/vis_manual.py
+++ NewPipeline/vis_manual.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 from plots.Individual_embeddings_plot import individual_embeddings_plot
 from plots.Merged_embeddings_plot import merged_embeddings_plot
-from NewPipeline.plots.set_plot_style import set_style
+from plots.set_plot_style import set_style
 
 import yaml
 with open('parameters.yaml', 'r') as file:
@@ -14,9 +14,11 @@ models = ['Specter2'] # , 'XLM_Roberta', 'MiniLm12'
 sources = ['pro', 'reg', 'sci']
 
 for model in models:
-    clustering = pd.read_csv(f'NewPipeline/clustering_outputs/{model}_clustering.csv')
+    merged_clusters = pd.read_csv(f'NewPipeline/clustering_outputs/{model}_merged_clustering.csv')
 
-    merged_embeddings_plot(clustering, model)
+    merged_embeddings_plot(merged_clusters, model)
+
+    clustering = pd.read_csv(f'NewPipeline/clustering_outputs/{model}_clustering.csv')
 
     for source in sources:
 
